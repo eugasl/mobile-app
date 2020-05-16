92f7413b9d12b43a8261286eb9ca33a935881d70
Eugene Aslanov <eugasl@gmail.com>
2020-05-16 15:21:28 +0300

    phone mask

--- components/Callback/index.js
+++ components/Callback/index.js
@@ -1,31 +1,51 @@
+import { useRef, useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import classnames from "classnames";
+import IMask from "imask";
 
 import styles from "./callback.module.css";
 
-const Callback = ({ withoutLabel, inContent }) => (
-  <div
-    className={classnames("", {
-      [styles.callback]: !withoutLabel,
-    })}
-  >
+const Callback = ({ withoutLabel, inContent }) => {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+  useEffect(() => {
+    IMask(target.current, {
+      mask: "+{7}(000)000-00-00",
+    });
+    document.addEventListener("click", ({ target }) => {
+      if (target.tagName === "INPUT") {
+        return false;
+      }
+      setShow(false);
+    });
+  });
+
+  return (
     <div
-      className={classnames(styles.callbackForm, {
-        [styles.callbackFormContent]: inContent,
+      className={classnames("", {
+        [styles.callback]: !withoutLabel,
       })}
     >
-      {!withoutLabel && (
-        <Form.Label className={styles.callbackLabel}>Телефон</Form.Label>
-      )}
-      <Form.Control
-        type="text"
-        placeholder="+7"
-        size="sm"
-        className={styles.callbackInput}
-      />
-      <button className={styles.button}>Подобрать</button>
+      <div
+        className={classnames(styles.callbackForm, {
+          [styles.callbackFormContent]: inContent,
+        })}
+      >
+        {!withoutLabel && (
+          <Form.Label className={styles.callbackLabel}>Телефон</Form.Label>
+        )}
+        <Form.Control
+          ref={target}
+          type="text"
+          placeholder="+7"
+          size="sm"
+          className={styles.callbackInput}
+        />
+        <button className={styles.button}>Подобрать</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Callback;
